a23a8eb700c4c21ff875f2c97cd9ea1cb52c91aa
Cranival <111667804+Cranival@users.noreply.github.com>
2022-08-27 20:13:57 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085895086040]
-│ • Telkomsel [081334177340]
+│ • Axis [083893144467]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/Nurutomo
-│ • https://saweria.co/ariffb
+│ • Gopay [083893144467]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
